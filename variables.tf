630742e5a58f6be7d02a7ad4ea46544637aa4301
Mahmoud Mohamed <139778227+Mahmoudmohamed811@users.noreply.github.com>
2025-05-25 14:47:17 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,18 @@ variable "mysql_db" {
 variable "mysql_user" {
   default = "root"
 }
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "session_token" {
+  description = "AWS session token"
+  type        = string
+}
