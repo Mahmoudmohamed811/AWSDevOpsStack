21667d1cb5a6412a0b6ebe2e4fe36b57b14b3359
Mahmoud Mohamed <139778227+Mahmoudmohamed811@users.noreply.github.com>
2025-05-25 14:46:39 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIA26X57EFAWVLU2IEJ"
-  secret_key = "F0L+t3O4ptzKkNLhs/95cdsYmGmJMhHAD2yTL7M7"
-  token      = "IQoJb3JpZ2luX2VjEC8aCXVzLXdlc3QtMiJHMEUCIHE9R9mbEmKNKedNH3Kk/a4xHEmt1BrYINH+ZsX18IUSAiEAwryCvOu46pdEYJo65ZZ+5yTHfm0luf6QcZcXOL8RlZIqxgII2P//////////ARAAGgw3NTMyMjU2MzgyMDkiDHbT4eNr0zuKkgCg1iqaAmt2HvKr25dyPL6lGAqqCvp76j995n+ywGFkz+w1SP5SPT1g9cBAT5qk7gjE2JvRqIVLYQHf7N00Qye+U1/yYK3G5I4KNjhXNBf48xuzSMm0RAgkfstiPvurNbkhRA8VXFjT727had0DnFEhVTQ/b0SmAyZbA/zpODbnGj47Bby5pMmUZgF1l2675904s6A7RPhu6DyvpWHvmQJw9mRpcQ6LH0amdzzG0AabTHwTYPTbwizdfxDOwg1tHcd2U0/rk8Qapw8pyDInoG7wpusKwhM5RVWFGUg+Q1ORIoT6qbA8ya3eNElpjdfV542No+/07Fx/tuM/twRW130Q3z9n91t6NXLvgxpdt66kw8tPmVWuvPcnivxqTnDvkTD5jIjBBjqdAQL7FwGOOvjzA4WJdlbiq38K1rM9nP4j2XqW8srDgaXqspB+ibHM9bN7mjIGJhCtF0lOe0YThdUA3IPOeUu2uYRzYwsyx2qz8TTOOGjb1u0N7spDiSO1g0U4Ud4gy+WJZyl/qQMXT6X4qUKs7Z9/UZPTAgXlbufyOwOcUzuEd/vvVHrjfrtYgXWz2Tsfa8a/mltqHP/W1JTddZ1XL54="
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token      = var.session_token
 }
